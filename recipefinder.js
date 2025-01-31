55a9c71412ed30674e45634cd53e45f40bfc75db
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-31 22:17:09 +0500

    get all ids through dcm

---
+++ recipefinder.js
@@ -0,0 +1,4 @@
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const recipeContainer = document.getElementById('recipeContainer');
+const savedRecipes = document.getElementById('savedRecipes');
