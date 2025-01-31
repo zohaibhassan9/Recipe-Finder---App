47f79eed0c9b64209944377f7589197848970bb8
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-31 22:35:48 +0500

    SearchRecipes functions , async await

--- recipefinder.js
+++ recipefinder.js
@@ -5,6 +5,15 @@ const savedRecipes = document.getElementById('savedRecipes');
 
 const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-function searchRecipes(){
-    
+async function searchRecipes(){
+    const query = searchInput.ariaValueMax.trim();
+
+    if(!query) return alert('Please enter a  recipe name');
+
+    recipeContainer.innerHTML = "loadig";
+    const response =  await fetch(API_URL + query);
+    const data = response.json();
+
+    displayRecipes(data.meals);
 }
+
