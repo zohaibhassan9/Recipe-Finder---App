c49d2f2460ca836c868c368a8741295b06e5607f
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-31 22:17:41 +0500

    API_URL inserted

--- recipefinder.js
+++ recipefinder.js
@@ -2,3 +2,6 @@ const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
 const recipeContainer = document.getElementById('recipeContainer');
 const savedRecipes = document.getElementById('savedRecipes');
+
+const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
